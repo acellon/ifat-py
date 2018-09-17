5834269ef005d33f47ce920cbd6c531e3b3a1b85
Adam Cellon <adam.cellon@gmail.com>
2018-09-17 10:45:34 -0400

    weekend updates - full network, not working

--- ifat.py
+++ ifat.py
@@ -15,7 +15,7 @@
 #     name: python
 #     nbconvert_exporter: python
 #     pygments_lexer: ipython3
-#     version: 3.6.4
+#     version: 3.6.0
 # ---
 
 # # Development Notebook for IFAT Simulator
@@ -27,20 +27,43 @@ import matplotlib.pyplot as plt
 # %matplotlib inline
 # -
 
+def visualise_connectivity(S):
+    Ns = len(S.source)
+    Nt = len(S.target)
+    figure(figsize=(10, 4))
+    subplot(121)
+    plot(zeros(Ns), arange(Ns), 'ok', ms=10)
+    plot(ones(Nt), arange(Nt), 'ok', ms=10)
+    for i, j in zip(S.i, S.j):
+        plot([0, 1], [i, j], '-k')
+    xticks([0, 1], ['Source', 'Target'])
+    ylabel('Neuron index')
+    xlim(-0.1, 1.1)
+    ylim(-1, max(Ns, Nt))
+    subplot(122)
+    plot(S.i, S.j, 'ok')
+    xlim(-1, Ns)
+    ylim(-1, Nt)
+    xlabel('Source neuron index')
+    ylabel('Target neuron index')
+
 MODE = 'adaptive'
+PARASITICS = True
 
 # +
 # Define various equations
 
 if MODE == 'adaptive':
     neuron_eq = '''
-        # dVm/dt = ((glm + parasitic) / Cm) * (Vm_r - Vm) : volt
-        dVm/dt = (glm / Cm) * (Vm_r - Vm) : volt
-        dVt/dt = (glt / Ct) * (Vt_r - Vt) : volt
+        dVm/dt = ((glm + gpar) / Cm) * (Vm_r - Vm)  : volt
+        dVt/dt = ((glt + gpar) / Ct) * (Vt_r - Vt)  : volt
+        
+        # dVm/dt = (glm / Cm) * (Vm_r - Vm) : volt
+        # dVt/dt = (glt / Ct) * (Vt_r - Vt) : volt
 
-        glm = flm * Cl                    : siemens
-        glt = flt * Cl                    : siemens
-        # parasitic = 1.0 / parasitic_leak_time * Cm
+        glm = flm * Cl                              : siemens
+        glt = flt * Cl                              : siemens
+        gpar = par_ctrl / par_leak_time * Cm : siemens
     '''
     reset_eq = '''
         Vm = Vm_r
@@ -81,14 +104,18 @@ Cl = 0.02 * pF
 W_vals  = np.array([5, 10, 20, 40, 80]) * 0.001 * pF
 Em_vals = np.array([0, 1/3, 2/3, 1]) * Vdd
 
+par_ctrl = True
+par_ctrl = float(PARASITICS)
+par_leak_time = 12.5 * ms
+
 # +
 # Model parameters
-Vm_r = 1 * volt
+Vm_r = 1.25 * volt
 flm  = 0 * kHz
 Csm  = W_vals[0]
 
 Vt_r = 3 * volt
-flt  = 0.2 * MHz
+flt  = 0 * MHz
 Cst  = 0 * pF
 
 N = 4
@@ -101,56 +128,53 @@ test = NeuronGroup(N, neuron_eq, threshold='Vm > Vt', reset=reset_eq, method='ex
 test.Vm = Vm_r
 test.Vt = Vt_r
 
-in_spk_times = np.arange(0,1,0.01) * second
+start_spk_times = np.arange(0,0.05,0.002) * second
+start_spk_inds  = np.zeros_like(start_spk_times)
+#spgen = SpikeGeneratorGroup(1,in_spk_inds,in_spk_times)
+
+in_spk_times = np.arange(0,1,0.0002) * second
 in_spk_inds  = np.zeros_like(in_spk_times)
+#spgen = PoissonGroup(1,rates=2*kHz)
 spgen = SpikeGeneratorGroup(1,in_spk_inds,in_spk_times)
 
 insyn = Synapses(spgen, test, syn_eq, on_pre=presyn_eq)
 insyn.connect()
 insyn.delay = "j * 0.2 * us"
-insyn.Em = Em_vals[-1]
-insyn.W = sum(W_vals)
-
-# +
-def visualise_connectivity(S):
-    Ns = len(S.source)
-    Nt = len(S.target)
-    figure(figsize=(10, 4))
-    subplot(121)
-    plot(zeros(Ns), arange(Ns), 'ok', ms=10)
-    plot(ones(Nt), arange(Nt), 'ok', ms=10)
-    for i, j in zip(S.i, S.j):
-        plot([0, 1], [i, j], '-k')
-    xticks([0, 1], ['Source', 'Target'])
-    ylabel('Neuron index')
-    xlim(-0.1, 1.1)
-    ylim(-1, max(Ns, Nt))
-    subplot(122)
-    plot(S.i, S.j, 'ok')
-    xlim(-1, Ns)
-    ylim(-1, Nt)
-    xlabel('Source neuron index')
-    ylabel('Target neuron index')
+insyn.Em = Em_vals[2]
+insyn.W = W_vals[3]# + W_vals[2] + W_vals[0]
 
 visualise_connectivity(insyn)
 
+startup = SpikeGeneratorGroup(1,start_spk_inds, start_spk_times)
+insyn2 = Synapses(startup, test, syn_eq, on_pre=presyn_eq)
+insyn2.connect(i=0, j=0)
+insyn2.Em = Em_vals[3]
+insyn2.W = sum(W_vals)
 
-# +
-# insyn = Synapses(spgen, test, syn_eq, on_pre=presyn_eq, multisynaptic_index='k')
-# insyn.connect(i=0,j=0,n=100)
-# insyn.delay='0.2*us+k*0.2*us'
-# insyn.Em = Em_vals[1]
-# insyn.W = Csm
-
-# +
-# exc_syn = Synapses(test, test, syn_eq, on_pre=presyn_eq)
-# exc_syn.connect('j==((i+1)%6)')
-# exc_syn.Em = '4*volt'
-# exc_syn.W = 30 * Csm
+# + {"scrolled": true}
+exc_syn = Synapses(test, test, syn_eq, on_pre=presyn_eq)
+exc_syn.connect('j==((i+1)%(4))')
+exc_syn.connect('j==i')
+exc_syn.Em = Em_vals[3]
+exc_syn.W = sum(W_vals) + W_vals[1]
 # -
+inh_syn = Synapses(test, test, syn_eq, on_pre=presyn_eq)
+inh_syn.connect('j==((i+2)%4)')
+inh_syn.connect('j==((i+3)%4)')
+inh_syn.Em = Em_vals[0]
+inh_syn.W = sum(W_vals)*2
+
+# +
+#visualise_connectivity(inh_syn)
+
+# +
+#visualise_connectivity(exc_syn)
+# -
+
 sp_mon = SpikeMonitor(test)
 vm_mon = StateMonitor(test, 'Vm', record=True)
 vt_mon = StateMonitor(test, 'Vt', record=True)
+ratemon = PopulationRateMonitor(test)
 
 # + {"scrolled": true}
 run(1*second)
@@ -158,21 +182,14 @@ run(1*second)
 # + {"scrolled": true}
 for i in range(N):
     plot(vm_mon.t, vm_mon.Vm[i],vt_mon.t, vt_mon.Vt[i])
+xlim([0,0.2])
 
 # + {"scrolled": true}
-plot(vm_mon.t, vm_mon.Vm[0],vt_mon.t, vt_mon.Vt[0]); xlim([0,0.01])
-
-# + {"scrolled": true}
-for i in range(6):
-    plot(vmon.t, vmon.Vmem[i])
-    
-xlim([0.098,0.102])
+plot(vm_mon.t, vm_mon.Vm[2]/volt,vt_mon.t, vt_mon.Vt[0]/volt); xlim([0,0.2]);
 # -
 
-plot(mon.t, mon.i,'.'); xlim([0.099,0.11])
+plot(sp_mon.t, sp_mon.i, '.'); xlim([0,1]); ylim([-1, N+1])
 
-insyn
-
-insyn.delay
+plot(ratemon.t/ms, ratemon.smooth_rate(width=20*ms)/Hz)
 
 
