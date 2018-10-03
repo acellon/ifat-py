7990f969c018b56546db897a5a6dcadab0a74f26
Adam Cellon <adam.cellon@gmail.com>
2018-10-03 11:31:05 -0400

    VCO tuning curve update

--- iscas-ifat.py
+++ iscas-ifat.py
@@ -15,7 +15,7 @@
 #     name: python
 #     nbconvert_exporter: python
 #     pygments_lexer: ipython3
-#     version: 3.6.0
+#     version: 3.6.4
 # ---
 
 # # ISCAS Paper Work
@@ -121,7 +121,7 @@ par_leak_time = 12.5 * ms
 
 # +
 # Model parameters
-Vm_r = 0.75 * volt
+Vm_r = 1 * volt
 flm  = 0 * kHz
 Csm  = W_vals[0]
 
@@ -137,7 +137,7 @@ alpha = sum(W_vals)
 mu1 = 0
 mu2 = 2*pi/3
 mu3 = pi
-sigma = 25 * pi/180
+sigma = 36 * pi/180
 
 # +
 start_scope()
@@ -167,11 +167,11 @@ inh2inh.Em = Em_vals[0]
 inh2inh.W = calc_weight(M,alpha,mu1,sigma).flatten()
 # -
 
-PoisIn = PoissonGroup(M,rates=1.5*kHz)
+PoisIn = PoissonGroup(M,rates=2.0*kHz)
 p2exc = Synapses(PoisIn, blair_exc, syn_eq, on_pre=presyn_eq)
 p2exc.connect('j==i')
 p2exc.Em = Em_vals[3]
-p2exc.W = W_vals[2] + W_vals[1] + W_vals[0]
+p2exc.W = W_vals[2] + W_vals[0]# + W_vals[0]
 
 i_spmon = SpikeMonitor(blair_inh)
 e_spmon = SpikeMonitor(blair_exc)
@@ -182,23 +182,76 @@ erate10 = PopulationRateMonitor(blair_exc[10:11])
 erate20 = PopulationRateMonitor(blair_exc[13:14])
 irate = PopulationRateMonitor(blair_inh[:1])
 
-run(5*second,report='text')
+store()
+poissonRates = arange(2.0,4.0,0.1)*kHz
+
+print(poissonRates)
+
+# + {"scrolled": true}
+#rateOuts = zeros((4,20))
+eRates0 = []
+eRates1 = []
+eRates10 = []
+eRates20 = []
+# -
+
+for poissonRate in poissonRates:
+    restore()
+    PoisIn.rates = poissonRate
+    #print(PoisIn.rates)
+    run(8*second,report='text')
+    eRates0.append(erate0.smooth_rate(width=1000*ms))
+    eRates1.append(erate1.smooth_rate(width=1000*ms))
+    eRates10.append(erate10.smooth_rate(width=1000*ms))
+    eRates20.append(erate20.smooth_rate(width=1000*ms))
 
 figure(figsize=(8,6))
-plot(e_spmon.t/second, e_spmon.i,'.'); xlim([0,0.51])
+plot(e_spmon.t/second, e_spmon.i,'.'); xlim([2,2.1])
 
-rateWidth = 200 * ms
-plot(erate0.t/second,   erate0.smooth_rate(width=rateWidth),
-     erate1.t/second,   erate1.smooth_rate(width=rateWidth),
-     erate10.t/second, erate10.smooth_rate(width=rateWidth),
-     erate20.t/second, erate20.smooth_rate(width=rateWidth))#,
-    # irate.t/second,     irate.smooth_rate(width=rateWidth))#
+rateWidth = 1000 * ms
+plot(erate0.t/second,   erate0.smooth_rate(width=rateWidth)/Hz,
+     erate1.t/second,   erate1.smooth_rate(width=rateWidth)/Hz,
+     erate10.t/second, erate10.smooth_rate(width=rateWidth)/Hz,
+     erate20.t/second, erate20.smooth_rate(width=rateWidth)/Hz)
+xlim([2,6])
 
 plot(irate.t/second,irate.smooth_rate(width=100*ms))
 
-plot(e_vmon.t, e_vmon.Vm[0]); #xlim([0.2,0.8])
-scatter(e_spmon.t[e_spmon.i==0],3.65*ones(len(e_spmon.t[e_spmon.i==0])),color='r')
+idx = 34
+plot(e_vmon.t, e_vmon.Vm[idx]); xlim([0.2,0.8])
+scatter(e_spmon.t[e_spmon.i==idx],3.65*ones(len(e_spmon.t[e_spmon.i==idx])),color='r')
 
+shape(eRates0)
 
+# +
+smooth_width = 1000*ms
+
+#avgRate = zeros_like(eRates0[0].smooth_rate(width=smooth_width))
+avgRate = zeros((20,80000))
+
+for i in range(20):
+    avgRate[i,:]  =  eRates0[i]#.smooth_rate(width=smooth_width)
+    avgRate[i,:] +=  eRates1[i]#.smooth_rate(width=smooth_width)
+    avgRate[i,:] += eRates10[i]#.smooth_rate(width=smooth_width)
+    avgRate[i,:] += eRates20[i]#.smooth_rate(width=smooth_width)
+    avgRate[i,:] = avgRate[i,:]/4
+# -
+
+plot(erate0.t/second, avgRate[0,:]/Hz,
+     erate0.t/second, avgRate[1,:]/Hz,
+     erate0.t/second, avgRate[2,:]/Hz,
+     erate0.t/second, avgRate[3,:]/Hz)
+
+for i in range(20):    
+    plot(erate0.t/second, avgRate[i,:]/Hz)
+xlim([1.5,6.5])
+
+shape(avgRate)
+
+meanRates = np.mean(avgRate[:,15000:65000], axis=1)
+stdRates  = np.std(avgRate[:,15000:65000], axis=1)
+
+ax = gca()
+ax.errorbar(poissonRates/1000, meanRates, yerr=stdRates)
 
 
